d8d04070b0cc4193c3e63e21be263a406c52fb7c
TheOrangeHoodie <supremethedev@gmail.com>
2025-03-29 16:06:58 +0100

    Added a button disable after ball is caught

--- uis.py
+++ uis.py
@@ -2,23 +2,29 @@ import nextcord
 from nextcord.ext import commands
 
 class CatchModal(nextcord.ui.Modal):
-    def __init__(self, ball):
+    def __init__(self, ball, button, view):
         super().__init__(
             "Catch a Kingsball!"
         )
 
         self.ball = ball
+        self.button = button
+        self.view = view
 
         self.mdInput = nextcord.ui.TextInput("Name")
         self.add_item(self.mdInput)
 
-    async def callback(self, interaction):
+    async def callback(self, interaction : nextcord.Interaction):
         if type(self.ball["name"]) == str:
             if self.ball["name"].lower() == self.mdInput.value.lower(): 
-                return await interaction.response.send_message(f"You catched a ball! {self.ball["name"]}")
+                self.button.disabled = True
+                await interaction.response.edit_message(view=self.view)
+                return await interaction.followup.send(f"{interaction.user.mention} You caught a ball! {self.ball["name"]}")
         else:
             if self.mdInput.value.lower() in self.ball["name"]: 
-                return await interaction.response.send_message(f"You catched a ball! {self.ball["display"]}")
+                self.button.disabled = True
+                await interaction.response.edit_message(view=self.view)
+                return await interaction.followup.send(f"{interaction.user.mention} You caught a ball! {self.ball["display"]}")
             
         return await interaction.response.send_message("Wrong name!")
 
@@ -30,5 +36,4 @@ class CatchView(nextcord.ui.View):
 
     @nextcord.ui.button(label="Catch me!", style=nextcord.ButtonStyle.blurple)
     async def catchme(self, button : nextcord.ui.Button, interaction : nextcord.Interaction):
-        await interaction.response.send_modal(CatchModal(self.ball))
-        button.disabled = True
+        await interaction.response.send_modal(CatchModal(self.ball, button, self))
