123fbb6dcf8be034dded6251060645ccde2cf8f6
TheOrangeHoodie <supremethedev@gmail.com>
2025-06-02 21:07:27 +0200

    Added the info command

--- cogs/commands/Testers.py
+++ cogs/commands/Testers.py
@@ -10,6 +10,29 @@ class Testers(commands.Cog):
     @commands.command("ping")
     async def ping(self, ctx : commands.Context):
         await ctx.send(f"Pong 🏓! Latency: {str(round(self.bot.latency * 1000))}ms")
+
+    @nextcord.slash_command(name="info", description="All the info about this bot", guild_ids=[1349739992926130248])
+    async def collection(self, interaction : nextcord.Interaction):
+        embed = nextcord.Embed(
+            description=f"""
+                Kingsdex Bot is based on the Ballsdex bot.
+                Made for The Color Kings discord server.
+            """,
+
+            color=nextcord.Color.orange()
+        )
+
+        embed.set_author(name=self.bot.user.name, icon_url=self.bot.user.avatar.url)
+        embed.set_footer(text="© TheOrangeHoodie 2025")
+
+        embed.add_field(name="Credits:", value="""
+            Ball art: @averagevol, @admiral_podushka
+            Scripter: @orange.hoodie
+                        
+            Special thanks to: Entire The Color Kings Server!
+        """)
+
+        await interaction.response.send_message(embed=embed)
         
 async def setup(bot):
     bot.add_cog(Testers(bot))
